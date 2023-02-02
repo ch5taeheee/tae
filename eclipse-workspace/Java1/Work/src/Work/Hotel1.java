13eee9eb97f23cd34ba01a8ce3a2d6a1c9ea3829
taeeee <ch5taeheee@gmail.com>
2023-02-02 19:12:51 +0900

    2023.02.02

--- eclipse-workspace/Java1/Work/src/Work/Hotel1.java
+++ eclipse-workspace/Java1/Work/src/Work/Hotel1.java
@@ -1,27 +1,33 @@
 package Work;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Hotel1 {
+	public static void main(String[] args) {
+		new Hotel1().HotelStart();
+	}
+
 	private Scanner scan;
 	private Map<String, Hotel> hotelInfo;
 
-	public void Hotel1Test() {
+	public Hotel1() {
 		scan = new Scanner(System.in);
 		hotelInfo = new HashMap<>();
 	}
 
 	public void displayMenu() {
-		System.out.println();
 
+		System.out.println();
+		System.out.println("******************************");
 		System.out.println("어떤 업무를 하시겠습니까?");
 		System.out.println("1. 체크인 2. 체크아웃 3. 객실상태 4. 업무종료");
 		System.out.println("******************************");
-		System.out.println("메뉴선택 => ");
+		System.out.print("메뉴선택 => ");
 
 	}
 
@@ -30,129 +36,135 @@ public class Hotel1 {
 		System.out.println("호텔 문을 열었습니다.");
 		System.out.println("******************************");
 		System.out.println();
-		System.out.println("******************************");
 
 		while (true) {
 			displayMenu(); // 메뉴 출력
 
-			int menuNum = scan.nextInt(); // 메뉴 번호 입력
+			int menuNum = scan.nextInt();
 
 			switch (menuNum) {
 
 			case 1:
 				checkin();
+				break;
 			case 2:
 				checkout();
+				break;
 			case 3:
 				roominfo();
+				break;
 			case 4:
 				end();
+				return; // 메서드 종료
 			default:
 				System.out.println("잘못 입력하셨습니다. 다시 입력하세요.");
 
 			}
+
 		}
 	}
-
-
 
 	private void checkin() {
 		System.out.println();
 		System.out.println("어느 방에 체크인 하시겠습니까?");
-		System.out.println("방 번호 입력: ");
+		System.out.print("방 번호 입력: ");
 		String roomNum = scan.next();
-		System.out.println("누구를 체크인 하시겠습니까?");
-		System.out.println("이름 입력: ");
-		String name = scan.next();
+		System.out.println();
 
-		// 방이 비워있는지 확인하기
 		if (hotelInfo.get(roomNum) != null) {
-			System.out.println(roomNum + "방에는 이미 사람이 있습니다.");
+			System.out.println(roomNum + "방은 이미 사람이 있습니다.");
 			System.out.println("다시 입력해주세요");
 			return;
 		}
+		System.out.println("누구를 체크인 하시겠습니까?");
+		System.out.print("이름 입력: ");
+		String name = scan.next();
+
+		Hotel h = new Hotel(name, roomNum);
+		hotelInfo.put(roomNum, h);
+		System.out.println();
 		System.out.println("체크인 되었습니다.");
 
 	}
-	
+
 	private void checkout() {
 		System.out.println();
 		System.out.println("어느 방을 체크아웃 하시겠습니까?");
-		System.out.println("방 번호 입력 => ");
+		System.out.print("방 번호 입력 => ");
+		System.out.println();
 		String roomNum = scan.next();
-		
-		if(hotelInfo.get(roomNum) ==null ) {
-			System.out.println(roomNum  + " 에는 체크인한 사람이 없습니다.");
-			System.out.println(" 다시 입력해주세요.");
+
+		// remove(key) 성공하면 value 값이 반환 b, 실패하면 null 이 반환
+		if (hotelInfo.remove(roomNum) == null) {
+			System.out.println(roomNum + " 방에는 체크인한 사람이 없습니다.");
+			System.out.println();
+			System.out.println("다시 입력해주세요.");
 			return;
 
-		} System.out.println("체크아웃 되었습니다.");
-		
+		}
+		hotelInfo.remove(roomNum);
+		System.out.println("체크아웃 되었습니다.");
+
 	}
+
 	private void roominfo() {
-		
-		Set<String> keyset = hotelInfo.keySet();
-		
-		if(keyset.size() == 0) {
+
+		Set<String> keySet = hotelInfo.keySet();
+
+		if (keySet.size() == 0) {
 			System.out.println("정보가 없습니다.");
-			
-		} else { 
-			Iterator<String> it = keyset.iterator();
-			
+
+		} else {
+			Iterator<String> it = keySet.iterator();
 			while (it.hasNext()) {
 				String roomNum = it.next();
 				Hotel h = hotelInfo.get(roomNum);
 				System.out.println();
-				System.out.println("방번호 : " + h. + ", 투숙객 : " + h.getName());
-				
+				System.out.println("방번호 : " + h.getRoomNum() + ", 투숙객 : " + h.getName());
+
 			}
 		}
-		
-		
-		
-		
-		
-		
 
-		
 	}
-	
+
 	private void end() {
 
-		
+		System.out.println("******************************");
+		System.out.println("호텔 문을 닫았습니다.");
+		System.out.println("******************************");
+
 	}
 
 }
 
-	class HotelCu {
-		private String name;
-		private String roomNum;
+class Hotel {
+	private String name;
+	private String roomNum;
 
-		public HotelCu(String name, String roomNum) {
-			super();
-			this.name = name;
-			this.roomNum = roomNum;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public String getRoomNum() {
-			return roomNum;
-		}
-
-		public void setRoomNum(String roomNum) {
-			this.roomNum = roomNum;
-		}
-
-		@Override
-		public String toString() {
-			return "HotelInfo [name=" + name + ", roomNum=" + roomNum + "]";
-		}
+	public Hotel(String name, String roomNum) {
+		super();
+		this.name = name;
+		this.roomNum = roomNum;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRoomNum() {
+		return roomNum;
+	}
+
+	public void setRoomNum(String roomNum) {
+		this.roomNum = roomNum;
+	}
+
+	@Override
+	public String toString() {
+		return "hotelInfo [name=" + name + ", roomNum=" + roomNum + "]";
+	}
+}
